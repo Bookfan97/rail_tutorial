782ede3b09c943fbf4cb21c8c6fb2a0961c0384b
Noah <sheltielover432@gmail.com>
2019-11-07 22:46:05 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless current_user?(@user)
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  private
+private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -55,15 +55,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     # Confirms the correct user.
     def correct_user
